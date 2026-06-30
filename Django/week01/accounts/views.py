0de73938e6566702471f53cba07a1e9e6cc71e8c
yexnsxo <yslove829@naver.com>
2026-07-01 00:58:34 +0900

    Feat: 로그인 기능 추가

--- Django/week01/accounts/views.py
+++ Django/week01/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from django.contrib.auth import authenticate, login as auth_login
 
 def signup(request):
   if request.method == "POST": # POST 요청 시
@@ -11,4 +12,16 @@ def signup(request):
     form = SignupForm()
   
   return render(request, "signup.html", {"form":form})
+
+def login(request):
+  if request.method == "POST":
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(request, username=username, password=password) # 사용자 인증
+    if user is not None:
+      auth_login(request, user) # 장고에서 제공하는 login 메소드
+      return redirect('main')
+    
+  return render(request, 'login.html') # 인증 실패 시 로그인 페이지를 렌더링
 
--- Django/week01/myproject/urls.py
+++ Django/week01/myproject/urls.py
@@ -5,4 +5,5 @@ from accounts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', views.signup, name='signup'),
+    path('', views.login, name='login')
 ]
